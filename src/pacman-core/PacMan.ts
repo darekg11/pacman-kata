2c0777dc50d4acac7498a55a469c5725c5253399
ninsy <kwojciechowski094@gmail.com>
2019-08-22 19:22:27 +0200

    ghosts

--- src/pacman-core/PacMan.ts
+++ src/pacman-core/PacMan.ts
@@ -1,14 +1,56 @@
+enum State {
+    Regular = "REGULAR",
+    Super = "SUPER",
+}
+
 class PacMan {
     lives: number = 10;
     points: number = 0;
     level: number = 0;
     ballCount: number = 0;
-    ghostCount: number = 0;
-    state: "REGULAR" | "SUPER";
+    ghostCount: Map<string, number>;
+    state: State;
     superTime: number = 0;
+    
+    constructor() {
+        this.ghostCount = new Map<string, number>();
+    }
+
+    public totalGhostCount(): number {
+        let sum = 0;
+        this.ghostCount.forEach((value, key) => {
+            sum += value;
+        })
+        return sum;
+    }
+
+    public eatGhost(name: string) {
+        if (!this.ghostCount.has(name)) {
+            this.ghostCount.set(name, 1);
+        } else {
+            let curr = this.ghostCount.get(name);
+            this.ghostCount.set(name, ++curr);
+        }
+
+        switch (this.state) {
+            case State.Regular: {
+                this.lives -= 1;
+                if (this.lives === 0) {
+                    this.points = 0;
+                }
+                break;
+            }
+            case State.Super: {
+                this.points += 10;
+                break;
+            }
+        }
+    }
+
     public whatAmILike(): string {
         return 'funny';
     }
 }
 
 export default PacMan;
+export { State };
--- test/PacMan.test.ts
+++ test/PacMan.test.ts
@@ -1,6 +1,64 @@
-import PacMan from '../src/pacman-core/PacMan';
+import PacMan, { State } from '../src/pacman-core/PacMan';
+
+let p;
+
+beforeEach(() => {
+    p = new PacMan();
+})
 
 test('basic', () => {
-    const pacman = new PacMan();
-    expect(pacman.whatAmILike()).toBe('funny');
+    expect(p.whatAmILike()).toBe('funny');
+});
+
+test('if regular, decrease life by one', () => {
+    p.state = State.Regular;
+
+    p.eatGhost('ghost1');
+
+    expect(p.lives).toEqual(9);
+    expect(p.points).toEqual(0);
+});
+
+test('if super, increase points by ten', () => {
+    p.state = State.Super;
+
+    p.eatGhost('ghost1');
+
+    expect(p.lives).toEqual(10);
+    expect(p.points).toEqual(10);
+});
+
+test('gets total count', () => {
+    p.state = State.Super;
+
+    p.eatGhost('1');
+    p.eatGhost('1');
+    p.eatGhost('1');
+
+    p.eatGhost('2');
+    p.eatGhost('2');
+
+    p.eatGhost('3');
+
+    expect(p.totalGhostCount()).toEqual(6);
+});
+
+test('increase count for given ghost', () => {
+    const ghostName = 'ghost1';
+    p.state = State.Super;
+
+    p.eatGhost(ghostName);
+    p.eatGhost(ghostName);
+    p.eatGhost(ghostName);
+
+    expect(p.ghostCount.get(ghostName)).toBe(3)
+});
+
+test('if regular and lives === 0, points = 0', () => {
+    p.state = State.Regular;
+    p.lives = 0;
+
+    p.eatGhost('ghost1');
+
+    expect(p.points).toBe(0)
 });
