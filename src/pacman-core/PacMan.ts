524439e9ca97c53a546a0ed5f4cfd98034d3099c
Dariusz Górak <dariusz.gorak@rspective.pl>
2019-08-22 18:29:48 +0200

    Pacman state

--- src/pacman-core/PacMan.ts
+++ src/pacman-core/PacMan.ts
@@ -1,4 +1,11 @@
 class PacMan {
+    lives: number = 10;
+    points: number = 0;
+    level: number = 0;
+    ballCount: number = 0;
+    ghostCount: number = 0;
+    state: "REGULAR" | "SUPER";
+    superTime: number = 0;
     public whatAmILike(): string {
         return 'funny';
     }
